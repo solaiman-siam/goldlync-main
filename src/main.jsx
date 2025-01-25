0b9ff74a35e11527b723bfe690bf36dd0e4c78b2
solaiman-siam <solaimansiam93@gmail.com>
2025-01-25 15:46:39 +0600

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 import ContextProvider from "./provider/ContextProvider";
